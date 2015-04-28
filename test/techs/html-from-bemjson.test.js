0dd1569bbed8cb2c5d51077772330c775c8e3839
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-04-28 11:18:17 +0300

    add tests for html-from-bemjson tech

---
+++ test/techs/html-from-bemjson.test.js
@@ -0,0 +1,42 @@
+var fs = require('fs'),
+    path = require('path'),
+    mock = require('mock-fs'),
+    TestNode = require('enb/lib/test/mocks/test-node'),
+    HtmlFromBemjsonTech = require('../../techs/html-from-bemjson');
+
+describe('html-from-bemjson', function () {
+    var node,
+        bhCoreFilename = require.resolve('bh/lib/bh.js'),
+        bhFilename = path.join(__dirname, '..', 'fixtures', 'html-from-bemjson', 'test.bh.js');
+
+    beforeEach(function () {
+        var scheme = {
+            blocks: {
+                'block.bh.js': 'module.exports = function(bh) {bh.match("block", function(ctx) {ctx.tag("a");});};'
+            },
+            bundle: {
+                'bundle.bemjson.js': '({block: \'block\'})',
+                'bundle.bh.js': fs.readFileSync(bhFilename, 'utf-8')
+            }
+
+        };
+
+        scheme[bhCoreFilename] = fs.readFileSync(bhCoreFilename, 'utf-8')
+
+        mock(scheme);
+
+        node = new TestNode('bundle');
+    });
+
+    it('must generate html', function () {
+        return node.runTechAndGetContent(
+            HtmlFromBemjsonTech
+        ).spread(function (html) {
+            html.toString().must.be('<a class="block"></a>');
+        });
+    });
+
+    afterEach(function () {
+        mock.restore();
+    });
+});
